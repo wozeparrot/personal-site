087c75b3926536c4aa2fe73acc14211cae5ea324
Woze Parrot <wozeparrot@gmail.com>
2023-07-28 01:15:05 -0400

    feat: initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+IFS=$'\n\t'
+
+# create build directory
+mkdir -p build
+
+# copy assets
+cp -r assets build/assets
+
+# copy styles
+cp *.css build/
+
+# copy index.html
+cp index.html build/index.html
+
+# replace all <component-<component-name> /> tags with the component's html from components/ using a for loop
+for component in components/*; do
+  component_name="$(basename "$component")"
+  component_name="${component_name%.*}"
+  echo "Replacing <component-$component_name/> with $component"
+  sed -i "/<component-$component_name\ *\/>/r"<(cat "$component") build/index.html
+  sed -i "/<component-$component_name\ *\/>/d" build/index.html
+done
