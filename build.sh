b01ccd84e6aaac2e2f0f5c272011eb30c878826d
Woze Parrot <wozeparrot@gmail.com>
2023-07-28 20:00:29 -0400

    feat: more pages

--- build.sh
+++ build.sh
@@ -11,14 +11,16 @@ mkdir -p build
 # copy styles
 cp *.css build/
 
-# copy index.html
-cp index.html build/index.html
+# copy html files
+cp *.html build/
 
 # replace all <component-<component-name> /> tags with the component's html from components/ using a for loop
 for component in components/*; do
   component_name="$(basename "$component")"
   component_name="${component_name%.*}"
   echo "Replacing <component-$component_name/> with $component"
-  sed -i "/<component-$component_name\ *\/>/r"<(cat "$component") build/index.html
-  sed -i "/<component-$component_name\ *\/>/d" build/index.html
+  for html_file in build/*.html; do
+    sed -i "/<component-$component_name\ *\/>/r"<(cat "$component") "$html_file"
+    sed -i "/<component-$component_name\ *\/>/d" "$html_file"
+  done
 done
